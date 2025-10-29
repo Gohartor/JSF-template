1635529b0a23bc2eae42c72b620938adf5edc6bc
gohartor.m <gohartour1417@gmail.com>
2025-10-29 18:22:18 +0330

    add persistence.xml

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,8 +1,6 @@
 package main.java.org.example;
 
 
-import jdk.nashorn.internal.objects.annotations.Setter;
-
 public class User {
 
     private Long id;
@@ -10,4 +8,42 @@ public class User {
     private String email;
     private String phoneNumber;
 
+    public User(Long id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
