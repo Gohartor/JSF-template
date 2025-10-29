d3e149e8a7aa6dc8010058a86608a54e996bb3f5
gohartor.m <gohartour1417@gmail.com>
2025-10-29 18:04:22 +0330

    add setting database

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,13 @@
-package org.example;
+package main.java.org.example;
+
+
+import jdk.nashorn.internal.objects.annotations.Setter;
 
 public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
 }
