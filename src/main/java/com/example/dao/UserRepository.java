dc82d967e3f27c5179eaaae9af5585f3c7521464
Gohartor <gohartour1417@gmail.com>
2025-10-31 20:38:53 +0330

    add user & dao

---
+++ src/main/java/com/example/dao/UserRepository.java
@@ -0,0 +1,51 @@
+package com.example.dao;
+
+import com.example.entity.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class UserRepository {
+
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public UserRepository() {
+        this.emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        this.em = emf.createEntityManager();
+    }
+
+    public User save(User user) {
+        em.getTransaction().begin();
+        if (user.getId() == null) {
+            em.persist(user);
+        } else {
+            user = em.merge(user);
+        }
+        em.getTransaction().commit();
+        return user;
+    }
+
+    public User findById(Long id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
+    }
+
+    public void delete(Long id) {
+        em.getTransaction().begin();
+        User user = findById(id);
+        if (user != null) {
+            em.remove(user);
+        }
+        em.getTransaction().commit();
+    }
+
+    public void close() {
+        if (em != null) em.close();
+        if (emf != null) emf.close();
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(length = 11)
+    private String phone;
+
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
